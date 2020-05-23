ee358d7068c41d1b38c27842317e801634f0b297
cactuaroid <solamiredoredore@gmail.com>
2020-05-23 13:10:35 +0900

    add solution and MefManager

---
+++ OpcUaChatServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OpcUaChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chat server is starting. Press 'Esc' to exit.");
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+        }
+    }
+}
